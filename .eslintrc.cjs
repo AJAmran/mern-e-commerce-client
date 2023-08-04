6779005afdd61f4158a861603ad86c52ea543115
Md Amran Hossen <mdamranhossen77@gmail.com>
2023-08-04 22:28:48 +0600

    firebase auth added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, node:true },
+  env: { browser: true, es2020: true, node:true, props: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
