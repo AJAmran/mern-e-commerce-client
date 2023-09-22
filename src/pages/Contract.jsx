1bfd21893d283952c06663ac173be222fda5f5f1
Md Amran Hossen <mdamranhossen77@gmail.com>
2023-09-22 23:54:53 +0600

    contact page added

--- src/pages/Contract.jsx
+++ src/pages/Contract.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { FaMapMarkerAlt, FaPhone, FaEnvelope } from "react-icons/fa";
 import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
 
 const ContactPage = () => {
   const fadeIn = {
     hidden: { opacity: 0 },
     visible: { opacity: 1 },
+  };
+
+  const { handleSubmit, reset, register } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data); // You can handle form submission here
+    reset(); // Reset the form after submission
   };
 
   return (
@@ -15,43 +23,46 @@ const ContactPage = () => {
       variants={fadeIn}
       className="container p-8 mx-auto"
     >
-      <div className="md:flex md:justify-between">
+      <div className="gap-6 md:flex md:justify-between md:items-center">
         {/* Contact Form */}
-        <div className="md:w-1/2">
+        <div className="p-6 rounded-lg shadow-lg bg-gradient-to-r from-blue-100 to-purple-100 md:w-1/2">
           <h2 className="mb-6 text-3xl font-semibold">Contact Us</h2>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-4">
-              <label htmlFor="name" className="block font-semibold text-gray-800">
-                Name
+              <label htmlFor="name" className="block font-semibold text-gray-700">
+                Name:
               </label>
               <input
                 type="text"
                 id="name"
                 name="name"
+                {...register("name")}
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
                 required
               />
             </div>
             <div className="mb-4">
-              <label htmlFor="email" className="block font-semibold text-gray-800">
-                Email
+              <label htmlFor="email" className="block font-semibold text-gray-700">
+                Email:
               </label>
               <input
                 type="email"
                 id="email"
                 name="email"
+                {...register("email")}
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
                 required
               />
             </div>
             <div className="mb-4">
-              <label htmlFor="message" className="block font-semibold text-gray-800">
-                Message
+              <label htmlFor="message" className="block font-semibold text-gray-700">
+                Message:
               </label>
               <textarea
                 id="message"
                 name="message"
                 rows="4"
+                {...register("message")}
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
                 required
               ></textarea>
@@ -74,17 +85,17 @@ const ContactPage = () => {
           <h2 className="mb-4 text-3xl font-semibold">Contact Details</h2>
           <div className="flex items-center mb-4">
             <FaMapMarkerAlt className="text-xl text-blue-500" />
-            <p className="ml-2 text-gray-800">
-              123 Main Street, Cityville, Country
+            <p className="ml-2 text-gray-700">
+              Dhaka, Bangladesh
             </p>
           </div>
           <div className="flex items-center mb-4">
             <FaPhone className="text-xl text-blue-500" />
-            <p className="ml-2 text-gray-800">+1 (123) 456-7890</p>
+            <p className="ml-2 text-gray-700">+1 (123) 456-7890</p>
           </div>
           <div className="flex items-center">
             <FaEnvelope className="text-xl text-blue-500" />
-            <p className="ml-2 text-gray-800">contact@example.com</p>
+            <p className="ml-2 text-gray-700">contact@example.com</p>
           </div>
         </div>
       </div>
@@ -93,10 +104,9 @@ const ContactPage = () => {
       <div className="mt-8">
         <h2 className="mb-4 text-3xl font-semibold">Our Location</h2>
         <div className="overflow-hidden bg-gray-200 rounded-lg">
-          {/* Replace the following iframe with an actual map */}
           <iframe
             title="Our Location"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12345.6789!2d-12.3456789!3d12.3456789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTLCsDU5JzI2LjQiTiAxMsKwMDAnNTMuOSJF!5e0!3m2!1sen!2sus!4v1234567890123!5m2!1sen!2sus"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d60225.99685615883!2d90.36595939252458!3d23.777218917066136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755aa57e7e51121%3A0xf3b4b17f164c1fd6!2sDhaka%2C%20Bangladesh!5e0!3m2!1sen!2sus!4v1641053832544!5m2!1sen!2sus"
             width="100%"
             height="300"
             frameBorder="0"
